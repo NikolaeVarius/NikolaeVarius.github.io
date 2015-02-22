9c2647a227afea1a205b0d1a95a60db0a88bfafa
Joseph Kim <joekim29@gmail.com>
2015-02-21 21:57:35 -0500

    added some color

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,5 @@
-// $('#topnavbar').affix({
-//     offset: {
-//         top: $('#banner').height()
-//     }   
-// });
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+});
