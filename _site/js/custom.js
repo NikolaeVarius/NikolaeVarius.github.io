dd61c1852f1e2f6332922abd64396914802fab1a
Joseph Kim <joekim29@gmail.com>
2015-02-24 21:29:12 -0500

    added some button animations

--- _site/js/custom.js
+++ _site/js/custom.js
@@ -1,5 +1,24 @@
-$(function () {
+$(function () { //this set of functions deals with the nav bar
+	var $navbutton = $('.navbar-collapse ul li a'); //set variable for individual nav buttons
+
     $('.navbar-collapse ul li a:not(.dropdown-toggle)').bind('click touchstart', function () {
             $('.navbar-toggle:visible').click();
     });
+
+    $navbutton.on("mouseover",function(){ //change color when mouse hover
+	 	$(this).css({
+	 		'background-color':'#1a237e'
+	 	});
+    });
+
+   	$navbutton.on("mouseout",function(){ //change color when mouse exit
+ 		$(this).css({
+ 			'background-color':'#0d113f'
+ 		});
+    });
+
+});
+
+$(function () { //this set of functions governs animations?
+
 });
