b4fdeecda2ccc235926d1e35d426c24c8fd5e7ff
Joseph Kim <joekim29@gmail.com>
2015-03-10 20:15:04 -0400

    refactored some animations

--- js/custom.js
+++ js/custom.js
@@ -1,35 +1,51 @@
 $(document).ready(function() { 
-	//this set of functions deals with the nav bar
-	var $navbutton = $('.navbar-collapse ul li a'); //set variable for individual nav buttons
+	//Global Variable Regarding Nav Buttons. Need to figure out how to properly deal with this
+	var $navbutton = $('.navbar-collapse ul li a');
 
     $('.navbar-collapse ul li a:not(.dropdown-toggle)').bind('click touchstart', function () { //this fixes weird interaction between mobile nav and desktop
             $('.navbar-toggle:visible').click();
     });
 
-	$navbutton.mouseover(function(){ //change text color when mouse hover over nav button
+    //top nav bar behavior
+	$navbutton.mouseover(function(){
 	 	$(this).css({
 	 		'color':'#486c96'
 	 	});
 	});
-
-   	$navbutton.mouseover(function(){ //change text color when mouse exit
+   	$navbutton.mouseover(function(){
  		$(this).css({
  			'color':'#e0e0e0'
  		});
     });
+
+    function navOnClick(){
+
+    }
+
+    function getNavIndex(){
+
+    }
+
 	//This animates the on-load card enter/exit from bottom animation. I love this animation, its BEAUTIFUL
-	$(".animate-panel").each(function (i) {
+	$(".animate-panel-start").each(function (i) {
     var $panel = $(this); 
 	    setTimeout(function() { 
-			$panel.addClass('animate-panel-loaded') }, 300*i);
+			$panel.addClass('animate-panel-loaded') }, 500*i);
 	 });
 
-	$(".animate-about-panel").each(function (i) {
+	$(".animate-about-panel-start").each(function (i) {
     	var $panel = $(this); 
 	    setTimeout(function() { 
-			$panel.addClass('animate-about-panel-loaded') }, 300*i);
+			$panel.addClass('animate-about-panel-loaded') }, 500*i);
 	 });
 
+	//Returns the index of the currently selected section
+	function getCurrentSelection(){
+		var $currentIndex;
+
+		$('div .resumeCard').each(i).hasClass
+
+	}
 
 	//This section deals with the special case of the aside bar on the resume page
 	//trying to figure out a good way to manage this"
@@ -42,9 +58,10 @@ $(document).ready(function() {
 			var $activepanel = $('#Profile');
 			var $siblingpanel = $activepanel.siblings();
 
-			$activepanel.addClass("active");
-			$activepanel.removeClass("deactive");
-			$siblingpanel.toggleClass("active",false);
+			$activepanel.addClass("animate-panel-loaded");
+			$activepanel.removeClass("animate-panel-start");
+			$siblingpanel.toggleClass("deactivate-panel",true);
+			$siblingpanel.toggleClass("animate-panel-loaded",false);
 
 		}
 		else if($sectionindex === 1){
@@ -52,37 +69,37 @@ $(document).ready(function() {
 			var $siblingpanel = $activepanel.siblings();
 
 			
-			$activepanel.addClass("active");
-			$activepanel.removeClass("deactive");
-			$siblingpanel.toggleClass("deactive",true);
-			$siblingpanel.toggleClass("active",false);
+			$activepanel.addClass("animate-panel-loaded");
+			$activepanel.removeClass("animate-panel-start");
+			$siblingpanel.toggleClass("deactivate-panel",true);
+			$siblingpanel.toggleClass("animate-panel-loaded",false);
 		}
 		else if($sectionindex === 2){
 			var $activepanel = $('#Skills');
 			var $siblingpanel = $activepanel.siblings();
 
-			$activepanel.addClass("active");
-			$activepanel.removeClass("deactive");
-			$siblingpanel.toggleClass("deactive",true);
-			$siblingpanel.toggleClass("active",false);
+			$activepanel.addClass("animate-panel-loaded");
+			$activepanel.removeClass("animate-panel-start");
+			$siblingpanel.toggleClass("deactivate-panel",true);
+			$siblingpanel.toggleClass("animate-panel-loaded",false);
 		}
 		else if($sectionindex === 3){
 			var $activepanel = $('#Projects');
 			var $siblingpanel = $activepanel.siblings();
 
-			$activepanel.addClass("active");
-			$activepanel.removeClass("deactive");
-			$siblingpanel.toggleClass("deactive",true);
-			$siblingpanel.toggleClass("active",false);
+			$activepanel.addClass("animate-panel-loaded");
+			$activepanel.removeClass("animate-panel-start");
+			$siblingpanel.toggleClass("deactivate-panel",true);
+			$siblingpanel.toggleClass("animate-panel-loaded",false);
 		}
 		else if($sectionindex === 4){
 			var $activepanel = $('#Other');
 			var $siblingpanel = $activepanel.siblings();
 
-			$activepanel.addClass("active");
-			$activepanel.removeClass("deactive");
-			$siblingpanel.toggleClass("deactive",true);
-			$siblingpanel.toggleClass("active",false);
+			$activepanel.addClass("animate-panel-loaded");
+			$activepanel.removeClass("animate-panel-start");
+			$siblingpanel.toggleClass("deactivate-panel",true);
+			$siblingpanel.toggleClass("animate-panel-loaded",false);
 		};
 
 	}); 
